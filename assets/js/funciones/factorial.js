a97e6f8daa5baf571278ee6371edc8a1fe2780de
santiagor06 <santiago.restrepo06@hotmail.com>
2023-09-28 17:37:10 -0500

    fix factorial 0

--- assets/js/funciones/factorial.js
+++ assets/js/funciones/factorial.js
@@ -1,7 +1,7 @@
 export const factorial=()=>{
     const number=+prompt("Ingrese un numero");
     if(number===0){
-        alert("El resultado es: 0")
+        alert("El resultado es: 1")
         return
     }
     let total=1;
